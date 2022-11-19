863b3ffbea3e2e35f0ba21843b09f48a6dcbc605
Vivek KUMAR <enigmaticvision@gmail.com>
2022-11-19 21:30:28 +0000

    First commit

---
+++ 1.PrimeNumber.py
@@ -0,0 +1,14 @@
+def primenumber(number):
+    prime_no=True
+    div=number-1
+    while div>1:
+        rem = (number%div)
+        if rem==0:
+            prime_no = False
+            break
+        else:
+            div-=1 #reduced the number by 1 to check if remainder is zero (for non-prime or not (prime)
+    return prime_no
+
+print(primenumber(17))
+print(primenumber(18))
